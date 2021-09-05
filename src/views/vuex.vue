a23f1430cd8a16af0f4dca63431366e81ac3b1a0
cs-mac <halfmoonvic@gmail.com>
2021-09-05 15:43:14 +0800

    init

---
+++ src/views/vuex.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// third party tools and libraries
+// local functions and variables
+// local components
+</script>
+
+<template>
+  <div>vuex component</div>
+</template>
+
+<style lang="scss" scoped>
+@import '~@/css/index';
+</style>
